bc2f7043792005542198e2cab2c5c99b8954a0bc
Jeka102 <kvashnin@me.com>
2022-03-26 12:39:28 +0500

    делал логин скрин

--- main.py
+++ main.py
@@ -2,150 +2,19 @@ import kivy
 kivy.require('2.1.0') # replace with your current kivy version !
 
 from kivy.app import App
-from kivy.uix.pagelayout import PageLayout
-from kivy.uix.label import Label
-from kivy.base import runTouchApp
 from kivy.lang import Builder
+from kivy.uix.floatlayout import FloatLayout
 
 
-kv = '''
-PageLayout:
-    GridLayout:
-        canvas:
-            Color:
-                rgba: 0/255., 0/255., 0/255., 1
-            Rectangle:
-                pos: self.pos
-                size: self.size
-        rows: 4
-        GridLayout:
-            canvas:
-                Color:
-                    rgba: 0/255., 0/255., 0/255., 1
-                Rectangle:
-                    pos: self.pos
-                    size: self.size
-            cols: 1
-            AsyncImage:
-                source: 'images/unknown.png'
-        GridLayout:
-            canvas:
-                Color:
-                    rgba: 0/255., 0/255., 0/255., 1
-                Rectangle:
-                    pos: self.pos
-                    size: self.size
-            cols: 2
-            Label:
-                text: 'Логин'
-            TextInput
-        GridLayout:
-            canvas:
-                Color:
-                    rgba: 0/255., 0/255., 0/255., 1
-                Rectangle:
-                    pos: self.pos
-                    size: self.size
-            cols: 2
-            Label:
-                text: 'Пароль'
-            TextInput        
-        GridLayout:
-            canvas:
-                Color:
-                    rgba: 0/255., 0/255., 0/255., 1
-                Rectangle:
-                    pos: self.pos
-                    size: self.size
-            cols: 1
-            Button:
-                text: 'Подключиться'
-            TextInput    
-    BoxLayout:
-        canvas:
-            Color:
-                rgba: 0/255., 0/255., 0/255., 1
-            Rectangle:
-                pos: self.pos
-                size: self.size
+Builder.load_file('MainLayout.kv')
 
-        orientation: 'vertical'
-        Label:
-            size_hint_y: None
-            height: 1.5 * self.texture_size[1]
-            text: 'Тут будут настройки и морда капсулера!'
-    BoxLayout:
-        canvas:
-            Color:
-                rgba: 109/255., 8/255., 57/255., 1
-            Rectangle:
-                pos: self.pos
-                size: self.size
+class MainLayout(FloatLayout):
+    def redirect_to_eve(self):
+        pass
 
-        orientation: 'vertical'
-        Label:
-            size_hint_y: None
-            height: 1.5 * self.texture_size[1]
-            text: 'Тут будет график капитала!'
-
-        Button:
-            text: 'test'
-            on_press: print("test")
-    BoxLayout:
-        canvas:
-            Color:
-                rgba: 109/255., 8/255., 57/255., 1
-            Rectangle:
-                pos: self.pos
-                size: self.size
-
-        orientation: 'vertical'
-        Label:
-            size_hint_y: None
-            height: 1.5 * self.texture_size[1]
-            text: 'Тут будет каталог товаров с ценами!'
-
-        Button:
-            text: 'test'
-            on_press: print("test")            
-    BoxLayout:
-        orientation: 'vertical'
-        canvas:
-            Color:
-                rgba: 109/255., 8/255., 57/255., 1
-            Rectangle:
-                pos: self.pos
-                size: self.size
-
-        Label:
-            text: 'page 2'
-
-        AsyncImage:
-            source: 'http://kivy.org/logos/kivy-logo-black-64.png'
-
-    GridLayout:
-        canvas:
-            Color:
-                rgba: 0/255., 0/255., 0/255., 1
-            Rectangle:
-                pos: self.pos
-                size: self.size
-
-        cols: 2
-        Label:
-            text: 'page 3'
-        AsyncImage:
-            source: 'http://kivy.org/slides/kivyandroid-thumb.jpg'
-        Button:
-            text: 'test'
-            on_press: print("test last page")
-        AsyncImage:
-            source: 'http://kivy.org/slides/kivypictures-thumb.jpg'
-        Widget
-        AsyncImage:
-            source: 'http://kivy.org/slides/particlepanda-thumb.jpg'
-'''
-
+class MobileTTool(App):
+    def build(self):
+        return MainLayout()
 
 if __name__ == '__main__':
-    runTouchApp(Builder.load_string(kv))
+    MobileTTool().run()
